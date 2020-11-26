f938a81a84f5f68b7e1e616ceff8eb71c7ebfdb8
luv8860 <luv8860@users.noreply.github.com>
2020-11-26 18:29:43 +0530

    third change

--- lib/screens/body.dart
+++ lib/screens/body.dart
@@ -20,38 +20,52 @@ class Body extends StatelessWidget {
                 borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(60),
                     topRight: Radius.circular(60))),
-                    child: ListView(
-                      children: [SizedBox(height: 50),
-                        Container(height:150,width:200,decoration: BoxDecoration(
-                          color: Colors.orange,
-                          borderRadius: BorderRadiusDirectional.all(Radius.circular(30))
-                        ),),
-                        SizedBox(height: 50),
-                        Container(height:150,width:200,decoration: BoxDecoration(
-                          color: Colors.orange,
-                          borderRadius: BorderRadiusDirectional.all(Radius.circular(30))
-                        ),),
-                        SizedBox(height: 50),
-                        Container(height:150,width:200,decoration: BoxDecoration(
-                          color: Colors.orange,
-                          borderRadius: BorderRadiusDirectional.all(Radius.circular(30))
-                        ),),
-                        SizedBox(height: 50),
-                        Container(height:150,width:200,decoration: BoxDecoration(
-                          color: Colors.orange,
-                          borderRadius: BorderRadiusDirectional.all(Radius.circular(30))
-                        ),),
-                        SizedBox(height: 50),
-                        Container(height:150,width:200,decoration: BoxDecoration(
-                          color: Colors.orange,
-                          borderRadius: BorderRadiusDirectional.all(Radius.circular(30))
-                        ),),
-                      ],
-                    ),
+            child: ListView(
+              children: [
+                SizedBox(height: 50),
+                ProductCard(imglink:'assets/bed.png',name:'Hello'),
+                SizedBox(height: 50),
+                ProductCard(imglink:'assets/bed2.png',name:'Hello'),
+                SizedBox(height: 50),
+                ProductCard(imglink:'assets/bed3.png',name:'Hello'),
+                SizedBox(height: 50),
+                ProductCard(imglink:'assets/bed4.png',name:'Hello'),
+                SizedBox(height: 50),
+                ProductCard(imglink:'assets/bed5.png',name:'Hello'),
+
+
+                                
+              ],
+            ),
           )
         ]))
-
       ],
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  final String imglink;
+  final String name;
+  ProductCard({Key key, @required this.imglink, @required this.name}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      focusColor: Colors.transparent,
+      hoverColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      child: Container(
+        height: 200,
+        width: 500,
+        decoration: BoxDecoration(
+            color: Colors.orange,
+            borderRadius: BorderRadiusDirectional.all(Radius.circular(30))),
+        child:Stack(children: [
+          Positioned(top:20,left:10,child: Text(name,style:TextStyle(color:Colors.white,fontSize: 30))),
+          Positioned(top:20,right:10,child: Image.asset(imglink,height:80)),
+        ],)
+      ),
+      onPressed: () {},
     );
   }
 }
